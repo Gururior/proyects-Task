fbacda7e14d79ff172800a982165fc20fead8815
gururior <jesmanuel9991@gmail.com>
2024-07-23 10:52:39 -0600

    Cambios en el .env y en el Users

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { LocalStrategy } from './local.strategy';
-import { JwtStrategy } from './jwt.strategy';
 import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
+import { LocalStrategy } from './local.strategy';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     UsersModule,
     PassportModule,
     JwtModule.register({
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      secret: process.env.JWT_SECRET || 'your_jwt_secret', 
+      signOptions: { expiresIn: '1hr' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
